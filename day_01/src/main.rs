8c1bf2629372c212d42db84e44e94d9a7c46f1ee
prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-03-31 17:46:24 +0530

    initial solution

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,3 +1,28 @@
+use std::{fs::File, io::Read, path::Path};
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("input.txt");
+    let mut file = File::open(&path).expect("file could not be opened");
+    let mut lines = String::new();
+    let _ = file.read_to_string(&mut lines);
+
+    let mut sum = 0;
+    for line in lines.lines() {
+        let digits: Vec<u32> = line
+            .chars()
+            .filter_map(|character| {
+                if character.is_numeric() {
+                    Some(character.to_digit(10).unwrap())
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        sum += format!("{}{}", digits.first().unwrap(), digits.last().unwrap())
+            .parse::<u32>()
+            .unwrap();
+    }
+
+    println!("{}", sum);
 }
